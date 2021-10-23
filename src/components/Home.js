761267806618f495c8d45acbbb1ac5e99a2339ed
DanteThuta <dantethuta97@gmail.com>
2021-10-23 16:15:41 +0630

    Movie Section Grid Formattion

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 //Components
 import HeroImage from "./HeroImage";
+import Grid from "./Grid";
 
 //Hooks
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -28,9 +29,14 @@ const Home = () => {
         <HeroImage
           image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
           title={state.results[0].original_title}
-          text= {state.results[0].overview}
+          text={state.results[0].overview}
         />
       ) : null}
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <div key={movie.id}>{movie.title}</div>
+        ))}
+      </Grid>
     </>
   );
 };
