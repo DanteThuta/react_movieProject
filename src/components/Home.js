102390079e7b699559b9ece40fac835fc0421f95
DanteThuta <dantethuta97@gmail.com>
2021-10-23 19:01:20 +0630

    Spinner Added

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
+import Spinner from "./Spinner";
 
 //Hooks
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -47,6 +48,7 @@ const Home = () => {
           />
         ))}
       </Grid>
+      <Spinner />
     </>
   );
 };
