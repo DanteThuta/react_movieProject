abd317a288aca7668998bef05ba6ef97caaa6d21
DanteThuta <dantethuta97@gmail.com>
2021-10-29 15:07:12 +0630

    Adding PropTypes to Components

--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //Components Section
 import Thumb from "../Thumb";
@@ -48,6 +49,10 @@ const MovieInfo = ({ movie }) => {
       </Content>
     </Wrapper>
   );
+};
+
+MovieInfo.propTypes = {
+  movie: PropTypes.object
 };
 
 export default MovieInfo;
